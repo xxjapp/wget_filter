916ea6b755db665ccf0adf3f9e0c3e8df6d9c716
nmaeda <nmaeda@keepdata.asia>
2019-10-18 00:53:37 +0900

    implement

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,23 @@
 //
 // Usage:
 //      function wg() { f=$(echo $(basename $@) | cut -d'?' -f1); wget --progress=bar:force --no-check-certificate "$@" -O "$f" 2>&1 | wget_filter; }
-//      cargo build && wget --progress=bar:force http://ipv4.download.thinkbroadband.com/5MB.zip -O ttt 2>&1 | target/debug/wget_filter
+//      cargo build && wget --progress=bar:force http://ipv4.download.thinkbroadband.com/5MB.zip -O /tmp/tmp.zip 2>&1 | target/debug/wget_filter
 //
 
 use std::io;
 use std::io::prelude::*;
 use std::str;
 
+enum Status {
+    Ready,
+    FoundCr,
+    End,
+}
+
 fn main() {
     let mut buffer = [0; 1024];
+    let mut process = vec![];
+    let mut status = Status::Ready;
 
     loop {
         let n = match io::stdin().read(&mut buffer[..]) {
@@ -28,30 +36,48 @@ fn main() {
             }
         };
 
-        handle_buffer(&buffer[..n])
+        for &c in buffer[..n].iter() {
+            handle_char(&mut status, c, &mut process);
+
+            match status {
+                Status::End => return,
+                _ => {}
+            }
+        }
     }
 }
 
-fn handle_buffer(buf: &[u8]) {
-    let mut v = vec![];
-
-    for (i, c) in buf.iter().enumerate() {
-        if *c == 0x0A || *c == 0x0D {
-            v.push(i);
+fn handle_char(status: &mut Status, c: u8, process: &mut std::vec::Vec<u8>) {
+    match status {
+        Status::Ready => {
+            if c == 0x0D {
+                *status = Status::FoundCr;
+                process.push(c);
+            }
         }
+        Status::FoundCr => {
+            if c == 0x0D {
+                print_process(process);
+                process.clear();
+                process.push(c);
+            } else if c == 0x0A {
+                *status = Status::End;
+                process.push(c);
+                print_process(process);
+            } else {
+                process.push(c);
+            }
+        }
+        _ => {}
     }
+}
 
-    println!("buf = {:?}, {}", buf, buf.len());
-    println!("v = {:?}\n", v);
-
-    // if let Some(0x0D) = buf.get(0) {
-    //     match str::from_utf8(buf) {
-    //         Ok(v) => {
-    //             print!("{}", v);
-    //             // print!("{:?}\n", v.lines());
-    //             io::stdout().flush().unwrap();
-    //         }
-    //         Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    //     };
-    // }
+fn print_process(buf: &[u8]) {
+    match str::from_utf8(buf) {
+        Ok(v) => {
+            print!("{}", v);
+            io::stdout().flush().unwrap();
+        }
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
 }
